5486bf76b1f9e60101b56b0ca8e753e4cc08e293
Officialravi888 <youremail@yourdomain.com>
2023-05-15 14:38:33 +0530

    Calculation

---
+++ src/main/java/org/example/Calculation.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Calculation {
+    public double addTwoNumbers(double a, double b) {
+        return a + b;
+    }
+
+    public double subtractTwoNumbers(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplyTwoNumbers(double a, double b) {
+        return a * b;
+    }
+
+    public double divideTwoNumbers(double a, double b) {
+        return a / b;
+    }
+
+    public double calculatePercentage(double a) {
+        return a / 100;
+    }
+
+}
