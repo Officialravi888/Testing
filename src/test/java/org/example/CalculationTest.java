f10f9037ca7f7839fb7f03d60336151a09bf2bce
Officialravi888 <youremail@yourdomain.com>
2023-05-15 14:49:19 +0530

    all method

--- src/test/java/org/example/CalculationTest.java
+++ src/test/java/org/example/CalculationTest.java
@@ -16,4 +16,18 @@ class CalculationTest {
     public void subtraction_isCorrect(){
         assertEquals(5,new Calculation().subtractTwoNumbers(8,3),0);
     }
+    @Test
+    public void multiplication_isCorrect() {
+        assertEquals(24, new Calculation().multiplyTwoNumbers(8,3),0);
+    }
+
+    @Test
+    public void division_isCorrect() {
+        assertEquals(3, new Calculation().divideTwoNumbers(9,3),0);
+    }
+
+    @Test
+    public void percentage_isCorrect() {
+        assertEquals(0.03, new Calculation().calculatePercentage(3),0);
+    }
 }
